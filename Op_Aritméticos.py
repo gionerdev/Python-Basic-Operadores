529ba8714917af94ce2a8a84c0768cc6b7c2fe3c
gioner.x <122662305+gionerx@users.noreply.github.com>
2023-02-27 18:01:11 -0500

    Create Op_Aritméticos.py
    Podran observar que en Python se suelen utilizar para operaciones matemáticas 7 tipos de operadores.

---
+++ Op_Aritméticos.py
@@ -0,0 +1,12 @@
+# Operadores Aritméticos
+# La aritmética se utiliza para hacer operaciones matetmáticas
+
+suma = 10 + 8 # sumar
+restar = 15 - 10 # restar
+multiplicar = 5 * 8 # multiplicar
+dividir = 12 / 15 # división (el cosiente es decimal)
+exponenciación = 2 ** 10 # Potenciar 
+módulo = 10 % 30 # modulo te devuelve el residuo de una división
+floor = 15 // 4 # divition floor, te retorna el valor más cercano en int   
+
+print()
