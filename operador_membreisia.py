3c7c902aa51ef4c61b9cadf0719dbeaefa5d39ca
gioner_dev <122662305+gionerx@users.noreply.github.com>
2023-03-02 13:24:09 -0500

    Create operador_membreisia.py

---
+++ operador_membreisia.py
@@ -0,0 +1,32 @@
+# Los operadores de membresia in, not in se utilizan para evaluar si un elemento pertenece o no a una colección de datos.
+# tambíen se utiliza para saber si un dato esta presente dentro de una variable
+
+nintendo_64 = ["SuperMario64", "Mario Kart", "Mario Paper"]
+
+
+print("SuperMario64" in nintendo_64] # True
+print("Mario Kart" not in nuntendo_64) # False
+      
+
+diccionario_gamecube = {
+
+    "name": 'PSP',
+    "model": '3000',
+    "colour": [grey, black, white],
+    "date": 2004,
+
+}
+      
+print("name" in diccionario_gamecube)
+      
+# Membresia de variables  
+      
+x = 50.0
+y = 12.0
+z = 2.0      
+
+# Ejemplo 1      
+print(50.0 in x)
+      
+# Ejemplo 2      
+print(12.0 in y)
